c9a600be90df47a9e9bf67e0e5e17c74b0a4dcd8
Feda <atlz2404@yandex.ru>
2026-09-13 23:40:12 +0300

    fix: правка пути

--- homework_1/palindrome/__init__.py
+++ homework_1/palindrome/__init__.py
@@ -1,5 +1,7 @@
 """Добавление функции в пакет."""
 
-from .palindrome_without_string import is_palindrome
+from homework_1.palindrome.palindrome_without_string import (
+    is_palindrome,
+)
 
 __all__ = ["is_palindrome"]
